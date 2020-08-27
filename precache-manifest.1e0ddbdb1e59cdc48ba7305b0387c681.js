eddf5cc374776085ef72d84f7fd67e8ceedc3149
Konstantin <kostyone1@gmail.com>
2020-08-27 19:04:21 +0800

    Updates

---
+++ precache-manifest.1e0ddbdb1e59cdc48ba7305b0387c681.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8aaf58e5876883ce51cf8f075005c4",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "e952471376dd35953135",
+    "url": "/todo-list-react/static/css/main.8fe2dd50.chunk.css"
+  },
+  {
+    "revision": "d294f8eeabae3e7f0ce3",
+    "url": "/todo-list-react/static/js/2.5ef8902d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list-react/static/js/2.5ef8902d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e952471376dd35953135",
+    "url": "/todo-list-react/static/js/main.ba1392f1.chunk.js"
+  },
+  {
+    "revision": "d2ce1cbae4c6bd42d20e",
+    "url": "/todo-list-react/static/js/runtime-main.dc5b2452.js"
+  },
+  {
+    "revision": "e204e0453a3a739f14cddc3afc5cfd6c",
+    "url": "/todo-list-react/static/media/bin.e204e045.svg"
+  },
+  {
+    "revision": "c767ee9866dfa29af5e41add4d5d3d71",
+    "url": "/todo-list-react/static/media/plus.c767ee98.svg"
+  },
+  {
+    "revision": "0fb99c7634495abe5eae1a7ab5291887",
+    "url": "/todo-list-react/static/media/tick.0fb99c76.svg"
+  }
+]);
